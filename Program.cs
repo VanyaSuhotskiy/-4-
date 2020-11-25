4030571504c484160581abb7fe93139f0a8aa233
VanyaSuhotskiy <71978876+VanyaSuhotskiy@users.noreply.github.com>
2020-11-25 10:01:14 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace лб_4_задание_1
+{
+    class ArrayExample
+    {
+        static void Main()
+        {
+            char[] letters = { 'y', 'a', 'r', 'o', 's', 'l', 'a', 'w', ' ', 'g', 'a', 'r', 'b', 'a', 'r', 'e', 'n', 'k', 'o' };
+            string name = "";
+            int[] a = new int[19];
+            for (int i = 0; i < letters.Length; i++)
+            {
+                name += letters[i];
+                a[i] = i + 1;
+                SendMessage(name, a[i]);
+            }
+            Console.ReadKey();
+        }
+        static void SendMessage(string name, int msg)
+        {
+            Console.WriteLine("Hello, " + name + "! Count to " + msg);
+        }
+    }
+}
+ 
